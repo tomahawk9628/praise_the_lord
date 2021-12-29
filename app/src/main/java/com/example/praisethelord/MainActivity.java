87c7bd6b92e151b5bfbcde3560e797f1c9751751
tomahawk9628 <dissac80@gmail.com>
2021-12-30 00:40:40 +0530

    Splash Screen

--- app/src/main/java/com/example/praisethelord/MainActivity.java
+++ app/src/main/java/com/example/praisethelord/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.praisethelord;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        textView = findViewById(R.id.textview);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        YoYo.with(Techniques.FadeIn)
+                .duration(1000)
+                .playOn(findViewById(R.id.textview));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(MainActivity.this, SongList.class);
+
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
